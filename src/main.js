4d4d3929dcb95cfdff28b079cc85c35562afe9df
Ismail <ismail@github.com>
2020-10-02 18:58:18 +0300

    show test

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App.vue';
+import VueSuggestion from './components/vue-suggestion.vue';
+
+Vue.config.productionTip = false;
+
+Vue.component('vue-suggestion', VueSuggestion);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
